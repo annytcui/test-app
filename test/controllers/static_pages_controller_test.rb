4ad69f49d94a1f1436a05f938a946a0df275544d
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-08 23:14:52 -0500

    Test for root route

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @base_title = "Panda Visas"
+  end
+
+  test "should get root" do
+    get root_path
+    assert_response :success
+  end
 
   test "should get home" do
     get static_pages_home_url
