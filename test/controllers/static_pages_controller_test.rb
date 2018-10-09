5291e4a9a9fc1ea18149b683b734e8b5b8c22102
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-08 22:59:36 -0500

    Add test for titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Panda Visas"
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get visas" do
     get static_pages_visas_url
     assert_response :success
+    assert_select "title", "Visas | #{@base_title}"
   end
 
   test "should get forms" do
     get static_pages_forms_url
     assert_response :success
+    assert_select "title", "Forms | #{@base_title}"
   end
 
   test "should get community" do
     get static_pages_community_url
     assert_response :success
+    assert_select "title", "Community | #{@base_title}"
   end
 
 end
