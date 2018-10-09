c574fe3c73757d028c2f29e697e765d630132f57
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-08 23:11:54 -0500

    Set the root route to the Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
+  root 'static_pages#home'
   get 'static_pages/visas'
-
   get 'static_pages/forms'
-
   get 'static_pages/community'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
