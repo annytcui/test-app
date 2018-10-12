0f2286897b1e9b6e51a51309d36dc9e8d1f902df
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-12 14:23:14 -0500

    Add password_digest column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181012190224) do
+ActiveRecord::Schema.define(version: 20181012192224) do
 
   create_table "users", force: :cascade do |t|
     t.string "firstname"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20181012190224) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
