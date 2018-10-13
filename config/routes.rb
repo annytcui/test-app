7a5a420cdad1a6eeb889f62ebbc430a023ecd599
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-12 21:10:37 -0500

    Generate Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
   get 'visas', to: 'static_pages#visas', as: :visas
   get 'forms', to: 'static_pages#forms', as: :forms
