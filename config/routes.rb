4418a6b101619edef22ed2c4e525fb38f18aa821
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-12 21:23:48 -0500

    Add signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
-  get 'visas', to: 'static_pages#visas', as: :visas
-  get 'forms', to: 'static_pages#forms', as: :forms
-  get 'community', to: 'static_pages#community', as: :community
+  get '/visas', to: 'static_pages#visas'
+  get '/forms', to: 'static_pages#forms'
+  get '/community', to: 'static_pages#community'
+  get '/signup', to: 'users#new'
   resources :users
 end
