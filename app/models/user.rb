a6d125e10dd51303b59631a09448c1a1576976b3
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-12 21:55:45 -0500

    Add picture to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { email.downcase! }
+  mount_uploader :picture, ProfilePictureUploader
   validates :firstname, presence: true, length: { maximum: 25 }
   validates :lastname, presence: true, length: { maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
