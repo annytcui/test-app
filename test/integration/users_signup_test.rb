04065dc86c2dcd653362f375c19283b69d636af1
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-15 22:57:58 -0500

    Add user signup test

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get root_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { firstname: "",
+                      lastname: "", email: "user@invalid",
+                      password: "foo",
+                      password_confirmation: "barbar" } }
+    end
+    assert_template 'static_pages/home'
+    assert_select 'div.invalid-feedback', count: 5
+  end
+
+  test "valid signup information" do
+    get root_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: { firstname: "Example",
+                    lastname: "User", email: "user@example.com",
+                    password: "password123",
+                    password_confirmation: "password123" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+end
