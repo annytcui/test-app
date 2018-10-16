adfa0775a249cd21ecf978d64b92f1cf8cdeffe3
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-16 11:16:39 -0500

    Add routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root 'static_pages#home'
   get '/visas', to: 'static_pages#visas'
   get '/forms', to: 'static_pages#forms'
   get '/community', to: 'static_pages#community'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
