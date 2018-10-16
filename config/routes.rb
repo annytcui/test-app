773051c9a79df9ea10a84cd7b5185a37562ad001
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-15 22:52:44 -0500

    Add signup route to post request

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/forms', to: 'static_pages#forms'
   get '/community', to: 'static_pages#community'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   resources :users
 end
