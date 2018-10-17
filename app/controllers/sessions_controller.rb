0d56c14519078eadcc4d52f198a889405e71ca0f
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-17 16:35:25 -0500

    Implement "remember me"

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
       log_in user
-      remember user
+      params[:remember_me] == '1' ? remember(user) : forget(user) 
       redirect_to user
     else
       render 'new'
