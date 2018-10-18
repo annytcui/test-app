fb68949d76648ebdc1d227dc52c9dc8911774061
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-18 16:38:26 -0500

    Add gender to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181017191722) do
+ActiveRecord::Schema.define(version: 20181018213046) do
 
   create_table "users", force: :cascade do |t|
     t.string "firstname"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20181017191722) do
     t.string "password_digest"
     t.string "picture"
     t.string "remember_digest"
+    t.integer "gender_cd"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
