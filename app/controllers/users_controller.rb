36ebe776f08581e97a4228c288ff887cc1f649a9
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-18 21:16:57 -0500

    Add user create and update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(update_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
+    end
+
+    def update_params
+      params.require(:user).permit(:firstname, :lastname, :gender, :birthday)
     end
 end
