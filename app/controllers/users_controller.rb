9481b0bfb176631ad8eb0cb73e54149f1459a437
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-19 09:58:08 -0500

    Add a logged_in_user before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -38,5 +39,14 @@ class UsersController < ApplicationController
 
     def update_params
       params.require(:user).permit(:firstname, :lastname, :gender, :birthday)
+    end
+
+    # Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        redirect_to root_url
+      end
     end
 end
