95cd634a2202279fe4bf79f58f4125b8cce97798
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-22 16:07:33 -0500

    Add account activation to user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:primary] = "Please check your email."
+      redirect_to root_url
     else
       render 'static_pages/home'
     end
