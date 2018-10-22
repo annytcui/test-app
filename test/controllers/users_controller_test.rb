b70e451c9cbb1d69a74ad5ba246a080ef6d85c5a
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-22 10:37:11 -0500

    Add test for the admin attribute

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -18,4 +18,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     patch user_path(@user), params: { user: { firstname: @user.firstname, lastname: @user.lastname } }
     assert_redirected_to root_url
   end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: { user: {
+              password: "password123",
+              password_confirmation: "password123",
+              admin: true } }
+    assert_not @other_user.reload.admin?
+  end
 end
