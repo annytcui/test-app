d2c5bc2f664c0356e621916cb1d650230efd137b
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-22 13:49:34 -0500

    Add destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def show
     @user = User.find(params[:id])
@@ -32,6 +33,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to current_user
+  end
+
   private
 
     def user_params
@@ -47,13 +53,18 @@ class UsersController < ApplicationController
     # Confirms a logged-in user.
     def logged_in_user
       unless logged_in?
-        redirect_to root_url
+        redirect_to login_url
       end
     end
 
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
+      redirect_to root_url unless @user == current_user
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
